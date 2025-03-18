efbc669bec9cbb0c019400e779c2560caf3869fd
Enrique Resendiz <k.ventus250@gmail.com>
2025-03-18 03:29:40 -0600

    Fix: Error on lack Sharp Library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import vue from '@astrojs/vue';
-
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@astrojs/vue';
+import netlify from '@astrojs/netlify';
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-
+  // output: 'server',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  // adapter: netlify({
+  //   imageCDN: false
+  // })
 });
